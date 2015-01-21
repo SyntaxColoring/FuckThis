d78b91d0d5ebadc95de1fd547066ef5396027e95
Max Marrone <SyntaxColoring@gmail.com>
2015-01-21 12:09:11 -0500

    Generate empty main().

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <string.h>
 #include <stdio.h>
 #include <errno.h>
 
@@ -20,17 +21,56 @@ size_t write_32(unsigned long data, FILE* stream)
 	        write_8(data,       stream));
 }
 
-int main(int argc, char** argv)
+size_t write_utf8_constant(const char* string, FILE* stream)
+{
+	size_t length = strlen(string);
+	return (write_8(1, stream) &&
+	        write_16(length, stream) &&
+	        fwrite(string, 1, length, stream));
+}
+
+size_t write_class_constant(unsigned int name_index, FILE* stream)
+{
+	return (write_8(7, stream) && write_16(name_index, stream));
+}
+
+int main(void)
 {
 	FILE* of = fopen("FuckJava.class", "w");
+	
 	if (!of)
 	{
 		perror("Error creating file");
 	}
+	
 	else
 	{
 		write_32(0xcafebabe, of); /* Magic number. */
 		write_16(0, of); /* Minor version. */
-		write_16(45, of); /* Major version. */
+		write_16(51, of); /* Major version. */
+		
+		write_16(7, of); /* Number of entries in constant pool plus one. */
+		write_utf8_constant("FuckJava", of);
+		write_class_constant(1, of);
+		write_utf8_constant("java/lang/Object", of);
+		write_class_constant(3, of);
+		write_utf8_constant("main", of);
+		write_utf8_constant("([Ljava/lang/String;)V", of);
+		
+		write_16(0x0001 | 0x0010, of); /* Access flags (public final). */
+		
+		write_16(2, of); /* This class. */
+		write_16(4, of); /* Super class. */
+		
+		write_16(0, of); /* Interfaces count. */
+		write_16(0, of); /* Fields count. */
+		                    
+		write_16(1, of); /* Methods count. */
+		write_16(0x0001 | 0x0008, of); /* Method flags (public static). */
+		write_16(5, of); /* Name index. */
+		write_16(6, of); /* Descriptor index. */
+		write_16(0, of); /* Attributes count. */
+		
+		write_16(0, of); /* Attributes count. */
 	}
 }
