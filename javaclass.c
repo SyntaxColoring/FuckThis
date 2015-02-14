a0bea0f7c8eeeb34f85521910f6d1c3d0d2200dc
Max Marrone <SyntaxColoring@gmail.com>
2015-02-13 20:47:59 -0500

    Refactor javaclass.c.

--- javaclass.c
+++ javaclass.c
@@ -4,10 +4,10 @@
 #include <string.h>
 #include <assert.h>
 
-#include "intwrite.h"
+#include "util.h"
 
 /* Represents any kind of constant in the class's runtime constant pool. */
-struct constant
+struct java_constant
 {
 	unsigned char tag; /* Type identifier. */
 	
@@ -33,23 +33,13 @@ static const unsigned int CONSTANT_FIELD         = 9;
 static const unsigned int CONSTANT_METHOD        = 10;
 static const unsigned int CONSTANT_NAME_AND_TYPE = 12;
 
-struct java_class_impl
-{
-	const char* name;
-	const char* super_name;
-	unsigned int access_flags;
-	
-	size_t constant_count;
-	struct constant* constants;
-};
-
 const unsigned int JAVA_CLASS_PUBLIC = 0x0001;
 const unsigned int JAVA_CLASS_STATIC = 0x0008;
 const unsigned int JAVA_CLASS_FINAL  = 0x0010;
 
 /* Returns nonzero if a is logically equal to b, assuming both a and b reside
    (or will reside) in the same constant pool. */
-static int constants_equal(const struct constant a, const struct constant b)
+static int constants_equal(const struct java_constant a, const struct java_constant b)
 {
 	if (a.tag != b.tag) return 0;
 	else if (a.tag == CONSTANT_UTF8)
@@ -61,29 +51,42 @@ static int constants_equal(const struct constant a, const struct constant b)
 		        a.data.refs.b == b.data.refs.b);
 }
 
-static void write_constant(const struct constant constant, FILE* const stream)
+static void write_constant(const struct java_constant constant, struct byte_buffer* const buffer)
 {	
-	write_8(constant.tag, stream);
+	bb_write_u1(buffer, constant.tag);
 	
 	if (constant.tag == CONSTANT_UTF8)
 	{
-		write_16(strlen(constant.data.utf8), stream);
-		fprintf(stream, "%s", constant.data.utf8);
+		const size_t length = strlen(constant.data.utf8);
+		bb_write_u2(buffer, length);
+		bb_write_array(buffer, (u1*)constant.data.utf8, length);
 	}
 	
 	else
 	{
-		write_16(constant.data.refs.a, stream);
+		bb_write_u2(buffer, constant.data.refs.a);
 		if (constant.tag != CONSTANT_CLASS)
-			write_16(constant.data.refs.b, stream);
+			bb_write_u2(buffer, constant.data.refs.b);
 	}
 }
 
-static unsigned int add_constant(struct java_class_impl* destination,
-                                 struct constant new_constant)
+static void write_method(struct java_class* class, size_t method_index, struct byte_buffer* const buffer)
+{
+	const struct java_method method = class->methods[method_index];
+	bb_write_u2(buffer, method.access_flags);
+	bb_write_u2(buffer, java_class_ref_utf8(class, method.name));
+	bb_write_u2(buffer, java_class_ref_utf8(class, method.type));
+	bb_write_u2(buffer, 0);
+}
+
+static unsigned int add_constant(struct java_class* destination,
+                                 struct java_constant new_constant)
 {
 	size_t index;
 	
+	if (!destination->constants)
+		destination->constants = malloc(sizeof(struct java_constant)*50);
+		
 	/* Return a reference to an existing equivalent constant, if one exists. */
 	for (index = 0; index < destination->constant_count; index++)
 		if (constants_equal(new_constant, destination->constants[index]))
@@ -94,94 +97,80 @@ static unsigned int add_constant(struct java_class_impl* destination,
 	return destination->constant_count;
 }
 
-java_class java_class_create(const char* const name,
-                             const char* const super_name,
-                             const unsigned int access_flags)
+unsigned int java_class_ref_utf8(struct java_class* context, const char* string)
 {
-	struct java_class_impl* result = malloc(sizeof(struct java_class_impl));
-	result->name = name;
-	result->super_name = super_name;
-	result->access_flags = access_flags;
-	result->constant_count = 0;
-	result->constants = malloc(sizeof(struct constant) * 50); /* Hope that's enough! */
-	return result;
-}
-
-unsigned int java_class_ref_utf8(java_class context, const char* string)
-{
-	struct constant constant;
+	struct java_constant constant;
 	constant.tag = CONSTANT_UTF8;
 	constant.data.utf8 = string;
 	return add_constant(context, constant);
 }
 
-unsigned int java_class_ref_class(java_class context, const char* name)
+unsigned int java_class_ref_class(struct java_class* context, const char* name)
 {
-	struct constant constant;
+	struct java_constant constant;
 	constant.tag = CONSTANT_CLASS;
 	constant.data.refs.a = java_class_ref_utf8(context, name);
 	return add_constant(context, constant);
 }
 
-unsigned int java_class_ref_name_and_type(java_class context,
+unsigned int java_class_ref_name_and_type(struct java_class* context,
                                           const char* name,
                                           const char* type)
 {
-	struct constant constant;
+	struct java_constant constant;
 	constant.tag = CONSTANT_NAME_AND_TYPE;
 	constant.data.refs.a = java_class_ref_utf8(context, name);
 	constant.data.refs.b = java_class_ref_utf8(context, type);
 	return add_constant(context, constant);
 }
 
-unsigned int java_class_ref_field(java_class context,
+unsigned int java_class_ref_field(struct java_class* context,
                                   const char* class_name,
                                   const char* field_name,
                                   const char* field_type)
 {
-	struct constant constant;
+	struct java_constant constant;
 	constant.tag = CONSTANT_FIELD;
 	constant.data.refs.a = java_class_ref_class(context, class_name);
 	constant.data.refs.b = java_class_ref_name_and_type(context, field_name, field_type);
 	return add_constant(context, constant);
 }
 
-unsigned int java_class_ref_method(java_class context,
+unsigned int java_class_ref_method(struct java_class* context,
                                    const char* class_name,
                                    const char* method_name,
                                    const char* method_type)
 {
-	struct constant constant;
+	struct java_constant constant;
 	constant.tag = CONSTANT_METHOD;
 	constant.data.refs.a = java_class_ref_class(context, class_name);
 	constant.data.refs.b = java_class_ref_name_and_type(context, method_name, method_type);
 	return add_constant(context, constant);
 }
 
-void java_class_write(java_class source, FILE* of)
+void java_class_write(struct java_class* source, FILE* of)
 {
 	size_t index;
-	size_t this_class = java_class_ref_class(source, source->name);
-	size_t super_class = java_class_ref_class(source, source->super_name);
+	struct byte_buffer entire_file = BYTE_BUFFER_EMPTY;
+	struct byte_buffer after_constant_pool = BYTE_BUFFER_EMPTY; 
 	
-	write_32(0xcafebabe, of); /* Magic number. */
-	write_16(0, of); /* Minor version. */
-	write_16(51, of); /* Major version. */
-
-	write_16(source->constant_count + 1, of);
+	bb_write_u2(&after_constant_pool, source->access_flags);
+	bb_write_u2(&after_constant_pool, java_class_ref_class(source, source->name));
+	bb_write_u2(&after_constant_pool, java_class_ref_class(source, source->super_name));
+	bb_write_u2(&after_constant_pool, 0); /* Interfaces count. */
+	bb_write_u2(&after_constant_pool, 0); /* Fields count. */
+	bb_write_u2(&after_constant_pool, source->method_count);
+	for (index = 0; index < source->method_count; index++)
+		write_method(source, index, &after_constant_pool);
+	bb_write_u2(&after_constant_pool, 0); /* Class attributes count. */
 	
+	bb_write_u4(&entire_file, 0xcafebabe); /* Magic number. */
+	bb_write_u2(&entire_file, 0); /* Minor version. */
+	bb_write_u2(&entire_file, 51); /* Major version. */
+	bb_write_u2(&entire_file, source->constant_count + 1);
 	for (index = 0; index < source->constant_count; index++)
-		write_constant(source->constants[index], of);
-
-	write_16(source->access_flags, of); /* Access flags (public final). */
-
-	write_16(this_class, of); /* This class. */
-	write_16(super_class, of); /* Super class. */
-
-	write_16(0, of); /* Interfaces count. */
-	write_16(0, of); /* Fields count. */
-	                
-	write_16(0, of); /* Methods count. */
-
-	write_16(0, of); /* Class attributes count. */
+		write_constant(source->constants[index], &entire_file);
+	bb_write_array(&entire_file, after_constant_pool.data, after_constant_pool.length);
+	
+	bb_write_to_stream(&entire_file, of);
 }
--- javaclass.h
+++ javaclass.h
@@ -3,34 +3,54 @@
 
 #include <stdio.h>
 
-typedef struct java_class_impl* java_class;
+struct java_constant;
+
+struct java_method
+{
+	const char* name;
+	const char* type;
+	unsigned int access_flags;
+	
+	unsigned char* bytecode;
+	unsigned int max_stack;
+	unsigned int max_locals;
+};
+
+struct java_class
+{
+	const char* name;
+	const char* super_name;
+	unsigned int access_flags;
+	
+	size_t constant_count;
+	struct java_constant* constants;
+	
+	size_t method_count;
+	struct java_method* methods;
+};
 
 extern const unsigned int JAVA_CLASS_PUBLIC;
 extern const unsigned int JAVA_CLASS_FINAL;
 extern const unsigned int JAVA_CLASS_STATIC;
 
-java_class java_class_create(const char* name,
-                             const char* super_name,
-                             unsigned int access_flags);
+void java_class_write(struct java_class* source, FILE* destination);
 
-void java_class_write(java_class source, FILE* destination);
-
-unsigned int java_class_ref_utf8(java_class context,
+unsigned int java_class_ref_utf8(struct java_class* context,
                                  const char* string);
 
-unsigned int java_class_ref_class(java_class context,
+unsigned int java_class_ref_class(struct java_class* context,
                                   const char* name);
 
-unsigned int java_class_ref_name_and_type(java_class context,
+unsigned int java_class_ref_name_and_type(struct java_class* context,
                                           const char* name,
                                           const char* type);
 
-unsigned int java_class_ref_field(java_class context,
+unsigned int java_class_ref_field(struct java_class* context,
                                   const char* class_name,
                                   const char* field_name,
                                   const char* field_type);
 
-unsigned int java_class_ref_method(java_class context,
+unsigned int java_class_ref_method(struct java_class* context,
                                    const char* class_name,
                                    const char* method_name,
                                    const char* method_type);
