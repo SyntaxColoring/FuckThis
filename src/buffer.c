4c8e56d9963f40ec00147b3102302817e4e82bc3
Max Marrone <SyntaxColoring@gmail.com>
2015-02-23 22:55:28 -0500

    Write a new buffer API.

---
+++ src/buffer.c
@@ -0,0 +1,101 @@
+#include "buffer.h"
+
+#include <stddef.h> /* For size_t. */
+#include <stdlib.h> /* For free(). */
+#include <string.h> /* For memcpy(). */
+#include <stdio.h> /* For FILE* and fwrite(). */
+#include "util.h" /* For bool, malloc_or_die() and realloc_or_die(). */
+
+struct buffer_implementation
+{
+	buffer_u1* data;
+	size_t length;
+	size_t capacity;
+};
+
+/* Ensures there is enough space for the given buffer to hold at least
+   additional_length more bytes, possibly triggering a reallocation.  The
+   buffer's length is incremented by additional_length.  The returned pointer
+   points to the beginning of the freshly reserved space. */
+static buffer_u1* buffer_extend(buffer handle, size_t additional_length)
+{
+	const size_t required_capacity = handle->length + additional_length;
+	if (handle->capacity < required_capacity)
+	{
+		const size_t new_capacity = required_capacity*2;
+		realloc_or_die(handle->data, new_capacity);
+		handle->capacity = new_capacity;
+	}
+	handle->length += additional_length;
+	return handle->data + handle->length - additional_length;	
+}
+
+/* Sets the given buffer to be empty. */
+static void buffer_reset(buffer handle)
+{
+	handle->data = NULL;
+	handle->length = 0;
+	handle->capacity = 0;
+}
+
+buffer buffer_create(size_t initial_capacity)
+{
+	buffer new_buffer = malloc_or_die(sizeof(struct buffer_implementation));
+	buffer_reset(new_buffer);
+	buffer_extend(new_buffer, initial_capacity);
+	return new_buffer;
+}
+
+void buffer_free(buffer handle)
+{
+	free(handle->data);
+	free(handle);
+}
+
+size_t buffer_length(buffer handle)
+{
+	return handle->length;
+}
+
+buffer_u1* buffer_publish(buffer handle)
+{
+	buffer_u1* const data = handle->data;
+	buffer_reset(handle);
+	return data;
+}
+
+void buffer_write_u1(buffer handle, buffer_u1 u1)
+{
+	buffer_extend(handle, 1)[0] = u1;
+}
+
+void buffer_write_u2(buffer handle, buffer_u2 u2)
+{
+	buffer_u1* const bytes = buffer_extend(handle, 2);
+	bytes[0] = u2 >> 8;
+	bytes[1] = u2;
+}
+
+void buffer_write_u4(buffer handle, buffer_u4 u4)
+{
+	buffer_u1* const bytes = buffer_extend(handle, 4);
+	bytes[0] = u4 >> 24;
+	bytes[1] = u4 >> 16;
+	bytes[2] = u4 >> 8;
+	bytes[3] = u4;
+}
+
+void buffer_write_array(buffer handle, const buffer_u1* array, size_t length)
+{
+	memcpy(buffer_extend(handle, length), array, length);
+}
+
+void buffer_write_buffer(buffer destination_handle, buffer source_handle)
+{
+	buffer_write_array(destination_handle, source_handle->data, source_handle->length);
+}
+
+bool buffer_write_to_stream(buffer handle, FILE* stream)
+{
+	return (fwrite(handle->data, 1, handle->length, stream) == handle->length);
+}
---
+++ src/buffer.h
@@ -0,0 +1,53 @@
+/* buffer.h - A growing buffer type. */
+
+#ifndef BUFFER_H_INCLUDED
+#define BUFFER_H_INCLUDED
+
+#include <stddef.h> /* For size_t. */
+#include <stdio.h> /* For FILE*. */
+#include "util.h" /* For bool. */
+
+/* Opaque handle type. */
+typedef struct buffer_implementation* buffer;
+
+/* Unsigned integer types big enough to hold at least 1, 2 or 4 bytes, respectively. */
+typedef unsigned char buffer_u1;
+typedef unsigned int  buffer_u2;
+typedef unsigned long buffer_u4;
+
+/* Creates a new buffer.  At least initial_capacity bytes can be written to the
+   new buffer before a reallocation occurs.  It's the caller's responsibility to
+   call buffer_free() when she is done with the buffer. */
+buffer buffer_create(size_t initial_capacity);
+
+/* Frees all the resources held by the given buffer.  It's undefined behavior
+   to call any buffer function on a freed buffer. */
+void buffer_free(buffer handle);
+
+/* Returns the number of bytes that have been written to the given buffer. */
+size_t buffer_length(buffer handle);
+
+/* Returns a pointer to all the data written to the given buffer.  The buffer will
+   relinquish its ownership of the data; it will be the caller's responsibility to
+   free() it.  A buffer is left empty after it is published, as if it were just
+   created.  Calling buffer_publish() does not absolve the caller of the
+   responsibility to call buffer_free() when she is done with the buffer. */
+buffer_u1* buffer_publish(buffer handle);
+
+/* Note: All buffer_write_*() functions can trigger reallocations. */
+
+void buffer_write_u1(buffer handle, buffer_u1 u1); /* Writes a single byte. */
+void buffer_write_u2(buffer handle, buffer_u2 u2); /* Writes two bytes. (big-endian). */
+void buffer_write_u4(buffer handle, buffer_u4 u4); /* Writes four bytes (big-endian). */
+
+/* Writes length bytes from array into the given buffer. */
+void buffer_write_array(buffer handle, const buffer_u1* array, size_t length);
+
+/* Copies the contents of the given source buffer into the given destination buffer. */
+void buffer_write_buffer(buffer destination_handle, buffer source_handle);
+
+/* Attempts to write the given buffer to the given stream and returns whether or
+   not the write succeeded. */
+bool buffer_write_to_stream(buffer handle, FILE* stream);
+
+#endif
