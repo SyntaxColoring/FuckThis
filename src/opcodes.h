5d2f452465b05e81a11b7326884527131d4baa2d
Max Marrone <SyntaxColoring@gmail.com>
2015-02-26 04:25:57 -0500

    Add OP_ICONST_N_MIN/MAX.

--- src/opcodes.h
+++ src/opcodes.h
@@ -6,6 +6,8 @@
 #define OP_CALOAD        0x34
 #define OP_CASTORE       0x55
 #define OP_ICONST_N(n)   (0x03+(n))
+#define OP_ICONST_N_MIN  -1
+#define OP_ICONST_N_MAX  5
 #define OP_SIPUSH        0x11
 #define OP_RETURN        0xb1
 #define OP_NEWARRAY      0xbc
