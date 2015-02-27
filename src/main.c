4e2fa81ed1d8ad09fd94513a40b12e7059e12f8f
Max Marrone <SyntaxColoring@gmail.com>
2015-02-26 19:40:03 -0500

    Clean up main.c.

--- src/main.c
+++ src/main.c
@@ -7,57 +7,82 @@
 #include "java.h"
 #include "codegen.h"
 
+static const char* const help_string =
+	"Fuck This: a Brainfuck compiler that produces Java binaries.\n"
+	"https://github.com/SyntaxColoring/FuckThis\n"
+	"Usage:\n"
+	"  ft input_file class_name\n"
+	"Arguments:\n"
+	"  input_file - The name of the Brainfuck source file.\n"
+	"  class_name - The name of the Java class to generate.\n";
+
+static bool user_needs_help(size_t argument_count, char** arguments)
+{
+	size_t index;
+	for (index = 1; index < argument_count; index++)
+		if (!strcmp(arguments[index], "-h") || !strcmp(arguments[index], "--h"))
+			return true;
+	return false;
+}
+
 int main(int argument_count, char** arguments)
 {
-	if (argument_count != 3 ||
-	    (argument_count >= 2 &&
-	     (!strcmp(arguments[1], "-h") ||
-	      !strcmp(arguments[1], "--help"))))
+	if (user_needs_help(argument_count, arguments) || argument_count != 3)
 	{
-		printf("Fuck This: a Brainfuck compiler that produces Java binaries.\n"
-		       "https://github.com/SyntaxColoring/FuckThis\n"
-		       "Usage:\n"
-		       "  %s input_file class_name\n"
-		       "Arguments:\n"
-		       "  input_file - The name of the Brainfuck source file.\n"
-		       "  class_name - The name of the Java class to generate.\n",
-		       arguments[0]);
+		printf(help_string);
 		return EXIT_SUCCESS;
 	}
 	
 	else
 	{
-		char* output_filename = malloc(strlen(arguments[2]) + sizeof(".class") - 1);
-		FILE* input = fopen(arguments[1], "r");
+		const char* const source_filename = arguments[1];
+		const char* const class_name = arguments[2];
+		
+		char* output_filename;
+		FILE* source;
 		FILE* output;
+		java_file file;
+		struct java_class* class;
 		
-		strcpy(output_filename, arguments[2]);
-		strcat(output_filename, ".class"); 
-		output = fopen(output_filename, "w");
+		output_filename = malloc_or_die(strlen(class_name)+sizeof(".class")-1);
+		strcpy(output_filename, class_name);
+		strcat(output_filename, ".class");
 		
-		if (!output || !input)
+		source = fopen(source_filename, "r");
+		if (!source)
 		{
-			perror("Error opening input or output file");
+			perror("Couldn't open source file");
 			return EXIT_FAILURE;
 		}
 		
-		else
+		output = fopen(output_filename, "w");
+		if (!output)
 		{
-			java_file file = java_create();
-			struct java_class* class = java_get_class(file);
-			
-			class->name = arguments[2];
-			class->super_name = "java/lang/Object";
-			class->access_flags = JAVA_ACCESS_PUBLIC | JAVA_ACCESS_FINAL;
-			class->method_count = 1;
-			class->methods = malloc(sizeof(struct java_method));
-			class->methods[0].name = "main";
-			class->methods[0].type = "([Ljava/lang/String;)V";
-			class->methods[0].access_flags = JAVA_ACCESS_PUBLIC | JAVA_ACCESS_STATIC;
-			generate_code(file, 0, input);
-			java_write(file, output);
-			
-			return EXIT_SUCCESS;
+			perror("Couldn't open output file for writing");
+			return EXIT_FAILURE;
 		}
+		
+		file = java_create();
+		class = java_get_class(file);
+		
+		class->name = class_name;
+		class->super_name = "java/lang/Object";
+		class->access_flags = JAVA_ACCESS_PUBLIC | JAVA_ACCESS_FINAL;
+		
+		class->method_count = 1;
+		class->methods = malloc_or_die(sizeof(struct java_method));
+		class->methods[0].name = "main";
+		class->methods[0].type = "([Ljava/lang/String;)V";
+		class->methods[0].access_flags = JAVA_ACCESS_PUBLIC | JAVA_ACCESS_STATIC;
+		
+		generate_code(file, 0, source);
+		
+		java_write(file, output);
+		
+		free(class->methods);
+		java_free(file);
+		free(output_filename);
+		
+		return EXIT_SUCCESS;
 	}
 }
